18d047bf3d61f7b36a813bd7af6096a7ecc934f5
dynasofthk <30386907+dynasofthk@users.noreply.github.com>
2019-03-25 11:27:18 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title> {{ config('app.name')}}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,9 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
 
+                  {{ config('app.name')}}
+                  {{ App::Version() }} {{ date("H:i:s") }}
+                </div>
+                
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
